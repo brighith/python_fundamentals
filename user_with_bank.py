e6c8d991fb499a481bcdce3d359896a1434f5764
Brighith Yousef <brighith.y@live.com>
2023-05-10 22:02:41 +0300

    up dating

--- user_with_bank.py
+++ user_with_bank.py
@@ -1,31 +1,3 @@
-class BankAccount:
-    def __init__(self, int_rate, balance):
-        self.int_rate = int_rate
-        self.balance = balance
-
-    def deposit(self, amount):
-        self.balance += amount
-        return self
-
-    def withdraw(self, amount):
-        if self.balance <= 0:
-            print("Insufficient balance")
-        else:
-            self.balance -= amount
-        return self
-
-    def display_account_info(self):
-        print(f"{self.balance}")
-        return self
-
-    def yield_interest(self):
-        if self.balance > 0:
-            self.balance *= (1+self.int_rate/100)
-        else:
-            print("your account balance is negative")
-        return self
-
-
 class User:
     def __init__(self, name, email_adress):
         self.name = name
@@ -49,14 +21,31 @@ class User:
         other_user.account.deposit(amount)
         return self.account
 
-    def example_method(self):
-        self.account.deposit(100)
-        print(self.account.balance)
 
+class BankAccount:
+    def __init__(self, int_rate, balance):
+        self.int_rate = int_rate
+        self.balance = balance
 
-acount1 = BankAccount(5, 600)
-acount2 = BankAccount(int_rate=4.5, balance=8000)
-acount1.deposit(500).deposit(637).deposit(800).withdraw(
-    400).yield_interest().display_account_info()
-acount2.deposit(700).deposit(9000).withdraw(400).withdraw(300).withdraw(
-    100).withdraw(50).yield_interest().display_account_info()
+    def deposit(self, amount):
+        self.balance += amount
+        return self
+
+    def withdraw(self, amount):
+        self.balance -= amount
+        if self.balance <= 0:
+            print("Insufficient funds")
+            self.balance -= 5
+
+        return self
+
+    def display_account_info(self):
+        print(f"{self.balance}")
+        return self
+
+    def yield_interest(self):
+        if self.balance > 0:
+            self.balance *= (1+self.int_rate/100)
+        else:
+            print("your account balance is negative")
+        return self
